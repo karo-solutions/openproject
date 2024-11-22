d6c4b34de0ca67ad5bc1e57419c90e537c13a956
Behrokh Satarnejad <b.satarnejad@openproject.com>
2024-11-22 05:49:44 +0100

    make fon-size configurable

--- app/components/work_packages/info_line_component.rb
+++ app/components/work_packages/info_line_component.rb
@@ -3,9 +3,10 @@
 class WorkPackages::InfoLineComponent < ApplicationComponent
   include OpPrimer::ComponentHelpers
 
-  def initialize(work_package:)
+  def initialize(work_package:, font_size: :small)
     super
 
     @work_package = work_package
+    @font_size = font_size
   end
 end
